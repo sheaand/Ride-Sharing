73dcedc2d125e30b5b2f702acdd3d9886040c019
Marco Schwiebert <mschwieb@0587361261.wireless.umich.net>
2016-10-31 10:14:02 -0400

    Initial Commit

---
+++ RideShare/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RideShare
+//
+//  Created by Marco Schwiebert on 10/31/16.
+//  Copyright © 2016 mschwiebert. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RideShare/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RideShare
+//
+//  Created by Marco Schwiebert on 10/31/16.
+//  Copyright © 2016 mschwiebert. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
